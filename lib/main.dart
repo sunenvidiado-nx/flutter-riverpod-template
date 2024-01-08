96cfe59d48c307e3ac64e444aca48c5b9e7b5e57
Sun Adriann Envidiado <sunadriann31@gmail.com>
2024-01-08 10:07:04 +0800

    refactor: ♻️ simplify main method (#21)

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,13 @@
 // coverage:ignore-file
 
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_template/features/app/app.dart';
 
 void main() {
-  runZonedGuarded(
-    () async {
-      // Add your initialization code here (e.g. Firebase, local storage, etc.)
+  // Place your initializers here
 
-      runApp(const ProviderScope(child: App()));
-    },
-    (error, stack) {
-      // Add your error handling code here (e.g. Sentry, Firebase Crashlytics, etc.)
-    },
+  runApp(
+    const ProviderScope(child: App()),
   );
 }
