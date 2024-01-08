7cf87f7542bb364ca83b77a0c67823951fcc305d
Sun Adriann Envidiado <sunadriann31@gmail.com>
2024-01-08 09:42:51 +0800

    refactor: ♻️ use `GetRecentPostsUseCase`

--- lib/features/home/application/recent_posts_provider.dart
+++ lib/features/home/application/recent_posts_provider.dart
@@ -1,11 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod_template/features/home/data/flutter_dev_api.dart';
+import 'package:flutter_riverpod_template/features/home/data/get_recent_posts_use_case.dart';
 
 final recentPostsProvider = FutureProvider.autoDispose(
   (ref) async {
-    return ref
-        .read(flutterDevApiProvider)
-        .getRecentPosts()
-        .then((r) => r.data.children);
+    final useCase = ref.read(getRecentPostsUseCaseProvider);
+    return useCase.run().then((r) => r.data.children);
   },
 );
--- lib/features/home/data/get_recent_posts_use_case.dart
+++ lib/features/home/data/get_recent_posts_use_case.dart
@@ -4,19 +4,20 @@ import 'package:flutter_riverpod_template/core/api_providers/dio_providers.dart'
 import 'package:flutter_riverpod_template/features/home/domain/recent_posts_response.dart';
 import 'package:retrofit/retrofit.dart';
 
-part 'flutter_dev_api.g.dart';
+part 'get_recent_posts_use_case.g.dart';
 
 @RestApi()
-abstract class FlutterDevApi {
-  factory FlutterDevApi(Dio dio) = _FlutterDevApi;
+abstract class GetRecentPostsUseCase {
+  factory GetRecentPostsUseCase(Dio dio) = _GetRecentPostsUseCase;
 
   @GET('/r/flutterdev.json')
-  Future<RecentPostsResponse> getRecentPosts({
+  Future<RecentPostsResponse> run({
     @Query('after') String? after,
     @Query('before') String? before,
   });
 }
 
-final flutterDevApiProvider = Provider.autoDispose((ref) {
-  return FlutterDevApi(ref.watch(redditDioProvider));
+final getRecentPostsUseCaseProvider = Provider.autoDispose((ref) {
+  final dio = ref.read(redditDioProvider);
+  return GetRecentPostsUseCase(dio);
 });
