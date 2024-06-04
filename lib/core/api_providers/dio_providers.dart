276a21cf2844f361e00f4258b09c32e69217df8e
Sun Envidiado <sunadriann31@gmail.com>
2024-06-04 10:57:35 +0800

    feat: ✨ Upgrade Flutter to 3.22 along with some dependencies (#41)

--- lib/core/api_providers/dio_providers.dart
+++ lib/core/api_providers/dio_providers.dart
@@ -1,12 +1,24 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 // coverage:ignore-start
 final baseDioProvider = Provider((ref) {
-  return Dio()
-    ..interceptors.add(
-      LogInterceptor(responseBody: true),
-    );
+  final dio = Dio();
+
+  if (kDebugMode) {
+    dio.interceptors.add(PrettyDioLogger(
+      requestHeader: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: false,
+      error: true,
+      compact: false,
+    ));
+  }
+
+  return dio;
 });
 // coverage:ignore-end
 
